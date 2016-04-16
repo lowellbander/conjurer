28d05d17a24a2f52892a0893e229a37df243812f
Lowell Bander <lowellbander@gmail.com>
2016-04-16 14:47:44 -0700

    reactive radius

--- src/js/Conjurer.js
+++ src/js/Conjurer.js
@@ -14,26 +14,25 @@ class Conjurer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: "foobar",
+            radius: 50,
         };
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({radius: event.target.value});
     }
     
     render() {
-    var radius = 30;  
-
-    return (
+      return (
         <div>
-            <svg>
-                <circle cx={50} cy={50} r={radius} fill="red" />
+            <svg width={1000} height={1000}>
+                <circle cx={500} cy={500} r={this.state.radius} fill="red" />
             </svg>
             <p>this is a p tag's text</p>
             <input
                 type="text"
-                value={this.state.value}
+                value={this.state.radius}
                 onChange={this.handleChange}
             />
             <p>state:</p>
