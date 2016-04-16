94726e4e18be42d8f27842c6cfd2503f471436b3
Lowell Bander <lowellbander@gmail.com>
2016-04-16 15:57:25 -0700

    circle moves with mouse

--- src/js/Conjurer.js
+++ src/js/Conjurer.js
@@ -15,19 +15,29 @@ class Conjurer extends React.Component {
         super(props);
         this.state = {
             radius: 50,
+            x: 50,
+            y: 50,
         };
         this.handleChange = this.handleChange.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
     }
 
     handleChange(event) {
         this.setState({radius: event.target.value});
     }
+
+    onMouseMove(event) {
+        this.setState({
+            x: event.clientX,
+            y: event.clientY
+        })
+    }
     
     render() {
       return (
-        <div>
-            <svg width={1000} height={1000}>
-                <circle cx={500} cy={500} r={this.state.radius} fill="red" />
+        <div onMouseMove={this.onMouseMove}>
+            <svg width={500} height={500}>
+                <circle cx={this.state.x} cy={this.state.y} r={this.state.radius} fill="red" />
             </svg>
             <p>this is a p tag's text</p>
             <input
