63fbd4f9acf70565cbc8f6a6a5edb13f5ed11902
Lowell Bander <lowellbander@gmail.com>
2016-04-17 11:14:27 -0700

    made my own rect class

--- src/js/Conjurer.js
+++ src/js/Conjurer.js
@@ -48,21 +48,53 @@ class Conjurer extends React.Component {
         this.setState({dragging: !this.state.dragging});
     }
     
+    /*
+     <div onMouseMove={this.onMouseMove} onClick={this.onClick}>
+     <circle cx={this.state.x} cy={this.state.y} r={this.state.radius} fill="red" />
+     */
     render() {
       return (
-        <div onMouseMove={this.onMouseMove} onClick={this.onClick}>
+        <div>
             <svg width={1000} height={500}>
-                <rect x="10" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+                <MyRect x={10} y={10} width={30} height={30}/>
                 <circle cx="25" cy="75" r="20" stroke="red" fill="transparent" stroke-width="5"/>
                 <line x1="10" x2="50" y1="110" y2="150" stroke="orange" fill="transparent" stroke-width="5"/>
-
-                <circle cx={this.state.x} cy={this.state.y} r={this.state.radius} fill="red" />
+                
+                <svg width={100} height={100}>
+                    <rect x="50" y="10" width="30" height="30" stroke="black" fill="transparent" stroke-width="5"/>
+                </svg>
             </svg>
             <p>{JSON.stringify(this.state)}</p>
         </div>
     );
   }
 }
+
+class MyRect extends React.Component {
+   
+   onClick() {
+       console.log("clicked rect!");
+   } 
+    
+   render() {
+       return (
+           <rect onClick={this.onClick}
+                 x={this.props.x}
+                 y={this.props.y}
+                 width={this.props.width}
+                 height={this.props.height}
+                 stroke="black" 
+                 fill="transparent" 
+                 stroke-width="5"/>
+       );
+   } 
+}
+MyRect.propTypes = {
+    x: React.PropTypes.number.isRequired,
+    y: React.PropTypes.number.isRequired,
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+};
 
 ReactDOM.render(
   <Conjurer />,
